34dc7f9b5cc4e5ac21eea373e3f89543a4720090
Mori062 <01morishi@gmail.com>
2023-09-17 15:32:43 +0900

    [Fix] $NOTHING -> exit_status 0

--- srcs/no_pipe_main.c
+++ srcs/no_pipe_main.c
@@ -6,7 +6,7 @@
 /*   By: morishitashoto <morishitashoto@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 01:40:22 by morishitash       #+#    #+#             */
-/*   Updated: 2023/09/16 16:11:42 by morishitash      ###   ########.fr       */
+/*   Updated: 2023/09/17 15:27:30 by morishitash      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,8 @@ void	no_pipe_main(t_parser *parser_head, t_data *data)
 		redirect_output(tmp_parser->output, data);
 	if (g_signal != 1 && tmp_parser->cmd != NULL)
 		select_commands(tmp_parser->cmd, data->env_head, data, PARENT);
+	else if (g_signal != 1 && tmp_parser->cmd == NULL)
+		data->exit_status = 0;
 	dup2_error_exit(stdin_fd, STDIN_FILENO);
 	dup2_error_exit(stdout_fd, STDOUT_FILENO);
 }
