f562269674274a38bb5f222b96404f4796f1db3d
ryhara <aaxouei52@gmail.com>
2023-09-17 17:06:02 +0900

    [Fix] >a <a

--- srcs/have_pipe_main.c
+++ srcs/have_pipe_main.c
@@ -6,7 +6,7 @@
 /*   By: ryhara <ryhara@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 01:49:10 by morishitash       #+#    #+#             */
-/*   Updated: 2023/09/17 16:23:45 by ryhara           ###   ########.fr       */
+/*   Updated: 2023/09/17 17:03:42 by ryhara           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,10 +55,10 @@ void	child_process(t_pid *pid_data, int cmd_num,
 		next_pipe(pid_data, cmd_num);
 	if (tmp_parser->prev != NULL)
 		prev_pipe(pid_data, cmd_num);
-	if (tmp_parser->input != NULL)
-		redirect_input(tmp_parser->input, data);
 	if (tmp_parser->output != NULL)
 		redirect_output(tmp_parser->output, data);
+	if (tmp_parser->input != NULL)
+		redirect_input(tmp_parser->input, data);
 	if (g_signal != 1 && tmp_parser->cmd != NULL)
 		select_commands(tmp_parser->cmd, data->env_head, data, CHILD);
 	else if (g_signal != 1 && tmp_parser->cmd == NULL)
--- srcs/no_pipe_main.c
+++ srcs/no_pipe_main.c
@@ -6,7 +6,7 @@
 /*   By: ryhara <ryhara@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 01:40:22 by morishitash       #+#    #+#             */
-/*   Updated: 2023/09/17 16:10:15 by ryhara           ###   ########.fr       */
+/*   Updated: 2023/09/17 17:03:31 by ryhara           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ void	no_pipe_main(t_parser *parser_head, t_data *data)
 	tmp_parser = parser_head;
 	stdin_fd = dup_error_exit(STDIN_FILENO);
 	stdout_fd = dup_error_exit(STDOUT_FILENO);
-	if (tmp_parser->input != NULL)
-		redirect_input(tmp_parser->input, data);
 	if (tmp_parser->output != NULL)
 		redirect_output(tmp_parser->output, data);
+	if (tmp_parser->input != NULL)
+		redirect_input(tmp_parser->input, data);
 	if (g_signal != 1 && tmp_parser->cmd != NULL)
 		select_commands(tmp_parser->cmd, data->env_head, data, PARENT);
 	else if (g_signal != 1 && tmp_parser->cmd == NULL)
